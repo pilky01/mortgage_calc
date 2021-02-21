85dbccbf05c34bf038ac2a6802c22340e9d48e93
Matt Blackwell <well_have_less_o_that_matey@hotmail.com>
2021-02-21 11:24:34 +0000

    further additions to user interface

--- mortgage_calc/mortgage_gui.py
+++ mortgage_calc/mortgage_gui.py
@@ -13,7 +13,8 @@ class Application(ttk.Frame):
     
     def create_application(self):
         self.input_frame = InputFrame(self, text = "Input Values")
-        self.input_frame.grid(row = 0, column = 0, columnspan = "2")
+        self.input_frame.grid(row = 0, column = 0, columnspan = "2",
+                              sticky = "ew")
         self.results_frame = ResultsFrame(self)
         self.results_frame.grid(row = 1, column = 0)
         self.data_frame = DataFrame(self)
@@ -34,8 +35,7 @@ class InputFrame(ttk.Labelframe):
         self.principal_entry = ttk.Entry(self, justify = "right")
         self.principal_entry.grid(row = 1, column = 0, columnspan = "2")
         # Term
-        self.term_label = ttk.Label(self, text = "Term (Years)",
-                                    anchor = "w")
+        self.term_label = ttk.Label(self, text = "Term (Years)", anchor = "w")
         self.term_label.grid(row = 2, column = 0, sticky = "ew")
         self.term_entry = ttk.Entry(self, justify = "right", width = "5")
         self.term_entry.grid(row = 2, column = 1, sticky = "e")
@@ -43,9 +43,38 @@ class InputFrame(ttk.Labelframe):
         self.interest_label = ttk.Label(self, text = "Interest Rate (%)",
                                         anchor = "w")
         self.interest_label.grid(row = 3, column = 0, sticky = "ew")
-        self.interest_entry = ttk.Entry(self, justify = "right",
-                                        width = "5")
+        self.interest_entry = ttk.Entry(self, justify = "right", width = "5")
         self.interest_entry.grid(row = 3, column = 1, sticky = "e")
+        # Introductory Rate
+        self.introductory_rate_labelframe_label_widget = ttk.Frame(self)
+        self.introductory_rate_label =\
+                      ttk.Label(self.introductory_rate_labelframe_label_widget,
+                                text = "Introductory Rate")
+        self.introductory_rate_label.grid(row = 0, column = 0)
+        self.introductory_rate_checkbutton =\
+                ttk.Checkbutton(self.introductory_rate_labelframe_label_widget,
+                                )
+        self.introductory_rate_checkbutton.grid(row = 0, column = 1)
+        self.introductory_rate_labelframe = ttk.Labelframe(self,
+                  labelwidget = self.introductory_rate_labelframe_label_widget)
+        self.introductory_rate_labelframe.grid(row = 0, column = 2,
+                                               sticky = "ns", rowspan = "4")
+        self.introductory_duration_label =\
+                                   ttk.Label(self.introductory_rate_labelframe,
+                                             text = "Duration (Months)")
+        self.introductory_duration_label.grid(row = 0, column = 0)
+        self.introductory_duration_entry =\
+                                   ttk.Entry(self.introductory_rate_labelframe,
+                                             )
+        self.introductory_duration_entry.grid(row = 0, column = 1)
+        self.introductory_rate_label =\
+                                   ttk.Label(self.introductory_rate_labelframe,
+                                             text = "Rate (%)")
+        self.introductory_rate_label.grid(row = 1, column = 0)
+        self.introductory_rate_entry =\
+                                   ttk.Entry(self.introductory_rate_labelframe,
+                                             )
+        self.introductory_rate_entry.grid(row = 1, column = 1)
 
 
 class ResultsFrame(ttk.Frame):
@@ -55,7 +84,75 @@ class ResultsFrame(ttk.Frame):
         self.create_results()
     
     def create_results(self):
-        pass
+        # Monthly Payment
+        self.monthly_payment_label = ttk.Label(self,
+                                               text = "Monthly Payment (£)",
+                                               )
+        self.monthly_payment_label.grid(row = 0, column = 0)
+        self.monthly_payment_var = tk.DoubleVar(self)
+        self.monthly_payment_result = ttk.Label(self,
+                                       textvariable = self.monthly_payment_var)
+        self.monthly_payment_result.grid(row = 1, column = 0)
+        # Total Amount
+        self.total_amount_label = ttk.Label(self,
+                                            text = "Total Amount Payable (£)",
+                                           )
+        self.total_amount_label.grid(row = 2, column = 0)
+        self.total_amount_var = tk.DoubleVar(self)
+        self.total_amount_result = ttk.Label(self,
+                                          textvariable = self.total_amount_var)
+        self.total_amount_result.grid(row = 3, column = 0)
+        # Total Interest
+        self.total_interest_label = ttk.Label(self,
+                                              text = "Total Interest Paid (£)",
+                                              )
+        self.total_interest_label.grid(row = 4, column = 0)
+        self.total_interest_var = tk.DoubleVar(self)
+        self.total_interest_result = ttk.Label(self,
+                                        textvariable = self.total_interest_var)
+        self.total_interest_result.grid(row = 5, column = 0)
+        # Borrowing Factor
+        self.borrowing_factor_label = ttk.Label(self,
+                                     text = "For each £ borrowed, you pay (£)",
+                                     )
+        self.borrowing_factor_label.grid(row = 6, column = 0)
+        self.borrowing_factor_var = tk.DoubleVar(self)
+        self.borrowing_factor_result = ttk.Label(self,
+                                      textvariable = self.borrowing_factor_var)
+        self.borrowing_factor_result.grid(row = 7, column = 0)
+        # Remaining Balance
+        self.remaining_balance_frame = ttk.Frame(self)
+        self.remaining_balance_frame.grid(row = 8, column = 0)
+        
+        self.remaining_balance_after_label =\
+                        ttk.Label(self.remaining_balance_frame, text = "After")
+        self.remaining_balance_after_label.grid(row = 0, column = 0)
+        
+        self.remaining_balance_years_entry =\
+                                        ttk.Entry(self.remaining_balance_frame,
+                                                  width = 3)
+        self.remaining_balance_years_entry.grid(row = 0, column = 1)
+        
+        self.remaining_balance_years_label =\
+                    ttk.Label(self.remaining_balance_frame, text = "years and")
+        self.remaining_balance_years_label.grid(row = 0, column = 2)
+        
+        self.remaining_balance_months_entry =\
+                                        ttk.Entry(self.remaining_balance_frame,
+                                                  width = 3)
+        self.remaining_balance_months_entry.grid(row = 0, column = 3)
+        
+        self.remaining_balance_months_label =\
+                                        ttk.Label(self.remaining_balance_frame,
+                                                  text = "months")
+        self.remaining_balance_months_label.grid(row = 0, column = 4)
+        
+        self.remaining_balance_label = ttk.Label(self,
+                                                 text = "the amount owed is")
+        self.remaining_balance_label.grid(row = 9, column = 0)
+        self.remaining_balance_var = tk.DoubleVar()
+        self.remaining_balance_result = ttk.Label(self,
+                                     textvariable = self.remaining_balance_var)
 
 
 class DataFrame(ttk.Notebook):
